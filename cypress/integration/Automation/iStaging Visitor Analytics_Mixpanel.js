c1f1b151c5a34f6100c4cc1ec0f826442a1098f7
Eric Zeng <eric@staging.com.tw>
2022-03-17 16:57:41 +0800

    change date

--- cypress/integration/Automation/iStaging Visitor Analytics_Mixpanel.js
+++ cypress/integration/Automation/iStaging Visitor Analytics_Mixpanel.js
@@ -11,7 +11,7 @@ it('Mixpanel', () => {
     cy.get('#input-23').type('03f89648-3453-477b-93b4-79af7f10a86f');
     cy.get('#input-28').click();
     cy.get(':nth-child(2) > :nth-child(2) > .v-btn').click();
-    cy.get(':nth-child(3) > :nth-child(3) > .v-btn').click();
+    cy.get(':nth-child(3) > :nth-child(4) > .v-btn').click();
     cy.get('.v-picker__actions > .primary--text').click();
 
     /* Dev */
