86af08825c4cede3ef755b66691a82aa7075d961
Vibrary <inori.ocean@gmail.com>
2022-04-08 14:43:22 +0800

    add `rimraf`

--- cypress/integration/Automation/vreditor-feature-site-k8s-US.js
+++ cypress/integration/Automation/vreditor-feature-site-k8s-US.js
@@ -13,6 +13,6 @@ it('vreditor-feature-site-k8s-US', () => {
         failOnStatusCode : false
     }).wait(3000);
 
-    cy.get(':nth-child(1) > .setting-main > div > .setting-input').clear().type('feature/220310-ar-maker-tag');
+    cy.get(':nth-child(1) > .setting-main > div > .setting-input').clear().type('feature/2d-399-marker-center');
     cy.get('#yui-gen1-button').click();
 })
